8c1daa9b02ef9fdaf0d3209333a3c73c82cecf2f
Kevin Llampa <91292771+Kivito3d@users.noreply.github.com>
2026-05-20 16:36:29 -0300

    Agregado DetalleProyecto y correcion listaproyectos - Llampa Kevin

--- src/components/ListaProyectos.jsx
+++ src/components/ListaProyectos.jsx
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-const ListaProyectos = ({ proyectos, onEliminar }) => {
-  return (
-    <section className="tarjet">
-      {proyectos.map((proyecto) => (
-        <article key={proyecto.id} className="Cont-Proy">
-          <h2>{proyecto.titulo}</h2>
-          <p>Categoría: {proyecto.categoria}</p>
-          <p>Estado: {proyecto.estado}</p>
-          <div className="ButtonsAction">
-            <button className="btn-delete" onClick={() => onEliminar(proyecto.id)}>
-              Eliminar
-            </button>
-          </div>
-        </article>
-      ))}
-    </section>
-  );
-};
-
-export default ListaProyectos;
-=======
 import ProyectoCard from "./ProyectoCard";
 
 const ListaProyectos = ({ proyectos, onEliminar }) => {
@@ -45,4 +23,3 @@ const ListaProyectos = ({ proyectos, onEliminar }) => {
 };
 
 export default ListaProyectos;
->>>>>>> origin/rama-fran
